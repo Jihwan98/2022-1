548252530a0d1863c89a9124df47f99e475c647b
Jihwan98 <76936390+Jihwan98@users.noreply.github.com>
2022-03-10 12:19:20 +0900

    Delete 220310 directory

--- 220310/turtle_penta.py
+++
@@ -1,5 +0,0 @@
-import turtle
-penta_turtle = turtle.Turtle()
-for count in range(5):
-         penta_turtle.forward(100)
-         penta_turtle.left(72)
--- 220310/turtle_star2.py
+++
@@ -1,9 +0,0 @@
-import turtle
-
-t = turtle.Turtle()
-
-for i in range(5):
-    t.forward(200)
-    t.left(144)
-    
-turtle.done()
