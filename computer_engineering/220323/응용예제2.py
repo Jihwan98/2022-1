29616bd04b53b64a5dc79f3d76177b0540e7f43d
Jihwan98 <76936390+Jihwan98@users.noreply.github.com>
2022-03-23 16:43:48 +0900

    Add files via upload

---
+++ computer_engineering/220323/응용예제2.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    inStr = input('문자열 입력 : ')
+
+    for i in range(len(inStr) -1, -1, -1):
+        print('%c' % inStr[i], end='')
+
+# if __name__ == "__main__" :
+#     inStr = input('문자열을 입력 -->')
+#     for i in range(len(inStr) - 1, -1, -1) :
+#         print('%c' % inStr[i], end = '')
